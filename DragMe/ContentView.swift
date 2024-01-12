a928b2395d3de190089986568529f65ff9da537d
Tim Mitra <tim@it-guy.com>
2024-01-12 15:28:10 -0500

    combine long press and drag gesture

--- DragMe/ContentView.swift
+++ DragMe/ContentView.swift
@@ -8,23 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var isPressed = false
-  @GestureState private var longPressTap = false
+  // for longpress
+  @GestureState private var isPressed = false
+  // for dragGetsure
+  @GestureState private var dragOffset = CGSize.zero
+  @State private var position = CGSize.zero
   var body: some View {
     VStack {
       Image(systemName: "star.circle.fill")
         .font(.largeTitle)
         .foregroundStyle(.tint)
-        .opacity(longPressTap ? 0.4 : 1.0)
-        .scaleEffect(isPressed ? 2.5 : 1.0)
         .animation(.easeInOut, value: 2.0)
+        .opacity(isPressed ? 0.5 : 1.0)
+        .offset(x: position.width + dragOffset.width, y: position.height + dragOffset.height)
         .gesture(
           LongPressGesture(minimumDuration: 1.0)
-            .updating($longPressTap, body: { (currentState, state, transaction) in
-                            state = currentState
-                        })
-            .onEnded({ _ in
-              self.isPressed.toggle()
+                          .updating($isPressed, body: { (currentState, state, transaction) in
+                              state = currentState
+                          })
+                          .sequenced(before: DragGesture())
+            .updating($dragOffset, body: { (value, state, transaction) in
+              
+              switch value {
+              case .first(true):
+                print("Tapping")
+              case .second(true, let drag):
+                state = drag?.translation ?? .zero
+              default:
+                break
+              }
+            })
+            .onEnded({ (value) in
+              guard case .second(true, let drag?) = value else { return }
+              self.position.height += drag.translation.height
+              self.position.width += drag.translation.width
             })
         )
     }
