580e1f515c66778c06f59f91a9557b3bd0e1e45b
Tim Mitra <tim@it-guy.com>
2024-01-12 14:46:58 -0500

    Initial Commit

---
+++ DragMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DragMe
+//
+//  Created by Tim Mitra on 2024-01-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DragMe/DragMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragMeApp.swift
+//  DragMe
+//
+//  Created by Tim Mitra on 2024-01-12.
+//
+
+import SwiftUI
+
+@main
+struct DragMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
