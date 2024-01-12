ac9becd00e1b40871558baef65ee0a504158359b
Tim Mitra <tim@it-guy.com>
2024-01-12 15:06:24 -0500

    image dims on press, but doesn't scale until long press

--- DragMe/ContentView.swift
+++ DragMe/ContentView.swift
@@ -8,17 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State private var isPressed = false
+  @GestureState private var longPressTap = false
+  var body: some View {
+    VStack {
+      Image(systemName: "star.circle.fill")
+        .font(.largeTitle)
+        .foregroundStyle(.tint)
+        .opacity(longPressTap ? 0.4 : 1.0)
+        .scaleEffect(isPressed ? 2.5 : 1.0)
+        .animation(.easeInOut, value: 2.0)
+        .gesture(
+          LongPressGesture(minimumDuration: 1.0)
+            .updating($longPressTap, body: { (currentState, state, transaction) in
+                            state = currentState
+                        })
+            .onEnded({ _ in
+              self.isPressed.toggle()
+            })
+        )
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
